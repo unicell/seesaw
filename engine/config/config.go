c93847bc3d0797be24a42d6ef83dbc841485b172
Qiu Yu <unicell@gmail.com>
2020-06-10 14:32:32 -0700

    HCModeTUN

--- engine/config/config.go
+++ engine/config/config.go
@@ -235,6 +235,8 @@ func protoToHealthcheck(p *pb.Healthcheck, defaultPort uint16) *Healthcheck {
 		hcMode = seesaw.HCModePlain
 	case pb.Healthcheck_DSR:
 		hcMode = seesaw.HCModeDSR
+	case pb.Healthcheck_TUN:
+		hcMode = seesaw.HCModeTUN
 	}
 	var hcType seesaw.HealthcheckType
 	switch p.GetType() {
